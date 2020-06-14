21987316e9778561135df545054cf65448ac920b
Kaotich00 <49861879+kaotich00@users.noreply.github.com>
2020-06-14 23:56:09 +0200

    Initial commit

---
+++ fwapi.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	mcpinger "github.com/Raqbit/mc-pinger"
+)
+
+var servers = make(map[string]mcpinger.Pinger)
+
+type Serben struct {
+	Online uint `json:"online"`
+	Max    uint `json:"max"`
+}
+
+func main() {
+	jsonServers, err := os.Open("servers.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bytesServers, err := ioutil.ReadAll(jsonServers)
+	jsonServers.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var tmp map[string]string
+	if err := json.Unmarshal([]byte(bytesServers), &tmp); err != nil {
+		log.Fatal(err)
+	}
+
+	for k, v := range tmp {
+		s := strings.Split(v, ":")
+		if len(s) != 2 {
+			log.Fatal("Malformed serben: ", s)
+		}
+		port, err := strconv.Atoi(s[1])
+		if err != nil {
+			log.Fatal("Invalid port: ", s[1])
+		}
+		log.Println("Found serben ", k, " with ip ", s[0], " on port ", port)
+		servers[k] = mcpinger.New(s[0], uint16(port))
+	}
+
+	for k, _ := range servers {
+		http.HandleFunc("/"+k, func(w http.ResponseWriter, r *http.Request) {
+			if pinger, ok := servers[r.URL.EscapedPath()[1:]]; ok {
+				info, _ := pinger.Ping()
+				s := &Serben{info.Players.Online, info.Players.Max}
+				json.NewEncoder(w).Encode(s)
+			} else {
+				w.WriteHeader(http.StatusNotFound)
+			}
+		})
+	}
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/forgottenworld/fwapi
+
+go 1.14
+
+require github.com/Raqbit/mc-pinger v0.0.0-20191220212142-2843d3222bd5
