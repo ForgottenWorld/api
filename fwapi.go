11f77708fd72ab62881da1cbaed0d466309fd736
Kaotich00 <49861879+kaotich00@users.noreply.github.com>
2020-06-16 21:05:59 +0200

    Fix content type and CORS headers

--- fwapi.go
+++ fwapi.go
@@ -71,6 +71,9 @@ func view(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusServiceUnavailable)
 			return
 		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
 		s := &Serben{info.Players.Online, info.Players.Max}
 		json.NewEncoder(w).Encode(s)
 		return
