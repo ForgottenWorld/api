831c8a43d3dffac5bc919642864f4c7930a7f761
Enduvar <70820452+Enduvar@users.noreply.github.com>
2021-02-28 20:09:57 +0100

    Switch to fiber

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.14
 
 require (
 	github.com/Raqbit/mc-pinger v0.0.0-20191220212142-2843d3222bd5
-	github.com/valyala/fasthttp v1.16.0
+	github.com/gofiber/fiber/v2 v2.5.0
 )
--- main.go
+++ main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
 	"time"
 
-	"github.com/valyala/fasthttp"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 
 	mcpinger "github.com/Raqbit/mc-pinger"
 )
 
 var servers = make(map[string]mcpinger.Pinger)
-var serverList []byte
+var serverList []string
 
 var apiKey string
 var panel string
-
-var lastRefresh time.Time
 
 func init() {
 	apiKey = os.Getenv("PTERO_API_KEY")
@@ -32,97 +35,70 @@ func init() {
 }
 
 func main() {
-	if err := fasthttp.ListenAndServe(":8001", fastHTTPHandler); err != nil {
-		log.Fatalf("Error in ListenAndServe: %s", err)
-	}
+	app := fiber.New(fiber.Config{
+		UnescapePath: true,
+	})
+	app.Use(cors.New())
+	app.Use(compress.New(compress.Config{Level: compress.LevelBestCompression}))
+	app.Use(limiter.New(limiter.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Path() != "/reload"
+		},
+		Max:        1,
+		Expiration: 10 * time.Second,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return "key"
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.SendStatus(fiber.StatusTooManyRequests)
+		},
+	}))
+	app.Use(cache.New(cache.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Path() == "/reload"
+		},
+		Expiration: 3 * time.Second,
+	}))
+
+	app.Get("/servers", list)
+	app.Get("/reload", reload)
+	app.Get("/server/:name", view)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendStatus(fiber.StatusOK)
+	})
+
+	app.Listen(":8001")
 }
 
-func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
-	ctx.Response.Header.SetContentType("application/json")
-	ctx.Response.Header.Set("Access-Control-Allow-Origin", "*")
-	ctx.Response.Header.Set("Access-Control-Allow-Methods", "GET")
-	ctx.Response.Header.Set("X-Frame-Options", "DENY")
-	ctx.Response.Header.Set("X-Content-Type-Options", "nosniff")
-	ctx.Response.Header.Set("X-XSS-Protection", "1; mode=block")
-	ctx.Response.Header.Set("Referrer-Policy", "no-referrer")
-	ctx.Response.Header.Set("Content-Security-Policy", "default-src 'none';")
-
-	path := ctx.Path()
-
-	if bytes.Equal(path, []byte("/")) {
-		ctx.Response.SetStatusCode(fasthttp.StatusOK)
-		return
-	}
-
-	if bytes.Equal(path, []byte("/servers")) {
-		listHandler(ctx)
-		return
-	}
-
-	if bytes.Equal(path, []byte("/refresh")) {
-		refreshHandler(ctx)
-		return
-	}
-
-	if bytes.HasPrefix(path, []byte("/server/")) {
-		viewHandler(ctx)
-		return
-	}
-
-	ctx.Error("Unsupported path", fasthttp.StatusNotFound)
+func list(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusOK).JSON(serverList)
 }
 
-func listHandler(ctx *fasthttp.RequestCtx) {
-	ctx.Response.SetBody(serverList)
-	ctx.Response.SetStatusCode(fasthttp.StatusOK)
-}
-
-func refreshHandler(ctx *fasthttp.RequestCtx) {
-	elapsed := time.Since(lastRefresh)
-	if elapsed.Seconds() < 10 { //nolint: go-mnd
-		ctx.Response.SetBody([]byte("[\"Cached\"]"))
-		ctx.Response.SetStatusCode(fasthttp.StatusFound)
-		return
-	}
-	lastRefresh = time.Now()
+func reload(c *fiber.Ctx) error {
 	if err := refresh(); err != nil {
-		log.Printf("Error in refreshHandler: %s", err)
-		ctx.Response.SetStatusCode(fasthttp.StatusInternalServerError)
-		return
+		return c.SendStatus(fiber.StatusInternalServerError)
 	}
-	ctx.Response.SetBody([]byte("[\"OK\"]"))
-	ctx.Response.SetStatusCode(fasthttp.StatusOK)
+	return c.SendStatus(fiber.StatusOK)
 }
 
-func viewHandler(ctx *fasthttp.RequestCtx) {
-	k := ctx.Path()[len("/server/"):]
-	if pinger, ok := servers[string(k)]; ok {
+func view(c *fiber.Ctx) error {
+	name := c.Params("name")
+	if pinger, ok := servers[name]; ok {
 		info, err := pinger.Ping()
 		if err != nil {
-			log.Println(err)
-			ctx.Response.Header.SetStatusCode(fasthttp.StatusServiceUnavailable)
-			return
-		}
-		if info == nil {
-			ctx.Response.Header.SetStatusCode(fasthttp.StatusServiceUnavailable)
-			return
+			return c.SendStatus(fiber.StatusServiceUnavailable)
 		}
 		serv := struct {
 			Online uint `json:"online"`
 			Max    uint `json:"max"`
 		}{info.Players.Online, info.Players.Max}
-
-		s, _ := json.Marshal(serv)
-		ctx.Response.SetBody(s)
-		ctx.Response.SetStatusCode(fasthttp.StatusOK)
-		return
+		return c.Status(fiber.StatusOK).JSON(serv)
 	}
-
-	ctx.Error("Unsupported path", fasthttp.StatusNotFound)
+	return c.SendStatus(fiber.StatusNotFound)
 }
 
 func refresh() error {
-	ss, err := getServers()
+	ss, err := loadServers()
 	if err != nil {
 		return err
 	}
@@ -148,7 +124,7 @@ func refresh() error {
 			continue
 		}
 		if _, ok := nodes[s.Attributes.Node]; !ok {
-			allocs, err := getAllocations(s.Attributes.Node)
+			allocs, err := loadAllocations(s.Attributes.Node)
 			if err != nil {
 				return err
 			}
@@ -164,29 +140,29 @@ func refresh() error {
 		}
 	}
 
-	serverList, err = json.Marshal(keys)
+	serverList = keys
 	return err
 }
 
-func getServers() ([]byte, error) {
-	req := fasthttp.AcquireRequest()
-	resp := fasthttp.AcquireResponse()
-	defer fasthttp.ReleaseRequest(req)
-	defer fasthttp.ReleaseResponse(resp)
-
-	req.SetRequestURI(panel + "api/application/servers")
-	req.Header.SetContentType("application/json")
+func loadServers() ([]byte, error) {
+	req, err := http.NewRequest(http.MethodGet, panel+"api/application/servers", nil)
+	req.Header.Set("Content-type", "application/json")
 	req.Header.Set("Authorization", "Bearer "+apiKey)
 
-	err := fasthttp.Do(req, resp)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return []byte{}, err
 	}
 
-	return resp.Body(), nil
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+	return body, nil
 }
 
-func getAllocations(node int) (map[int]mcpinger.Pinger, error) {
+func loadAllocations(node int) (map[int]mcpinger.Pinger, error) {
 	jrsp := struct {
 		Data []struct {
 			Attributes struct {
@@ -197,7 +173,7 @@ func getAllocations(node int) (map[int]mcpinger.Pinger, error) {
 			} `json:"attributes"`
 		} `json:"data"`
 	}{}
-	b, err := getAllocs(node)
+	b, err := loadAllocs(node)
 	if err != nil {
 		return nil, err
 	}
@@ -213,25 +189,25 @@ func getAllocations(node int) (map[int]mcpinger.Pinger, error) {
 		}
 		allocs[a.Attributes.ID] = mcpinger.New(a.Attributes.IP, a.Attributes.Port)
 	}
-
 	return allocs, nil
 }
 
-func getAllocs(node int) ([]byte, error) {
+func loadAllocs(node int) ([]byte, error) {
 	n := strconv.Itoa(node)
-	req := fasthttp.AcquireRequest()
-	resp := fasthttp.AcquireResponse()
-	defer fasthttp.ReleaseRequest(req)
-	defer fasthttp.ReleaseResponse(resp)
 
-	req.SetRequestURI(panel + "api/application/nodes/" + n + "/allocations")
-	req.Header.SetContentType("application/json")
+	req, err := http.NewRequest(http.MethodGet, panel+"api/application/nodes/"+n+"/allocations", nil)
+	req.Header.Set("Content-type", "application/json")
 	req.Header.Set("Authorization", "Bearer "+apiKey)
 
-	err := fasthttp.Do(req, resp)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return []byte{}, err
 	}
 
-	return resp.Body(), nil
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+	return body, nil
 }
